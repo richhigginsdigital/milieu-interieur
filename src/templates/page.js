69c4641fdbeebccbd0ec1d9f1ef68f8142f22185
richhiggins <hello@iamrichhiggins.com>
2021-03-09 10:25:06 +0000

    initial work with new gatsby image plugin/rich text

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,16 +1,23 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
-import { BLOCKS, INLINES } from "@contentful/rich-text-types"
+import { BLOCKS } from "@contentful/rich-text-types"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ComponentName = ({ data }) => (
+const Page = ({ data }) => (
   <Layout>
     <SEO title={data.contentfulPage.title} />
     <h1>{data.contentfulPage.title}</h1>
-    {renderRichText(data.contentfulPage.mainContent, {})}
+    {renderRichText(data.contentfulPage.mainContent, {
+      renderNode: {
+        [BLOCKS.EMBEDDED_ASSET]: node => {
+          return <GatsbyImage image={node.data.target.gatsbyImageData} />
+        },
+      },
+    })}
   </Layout>
 )
 
@@ -22,9 +29,16 @@ export const query = graphql`
       slug
       mainContent {
         raw
+        references {
+          ... on ContentfulAsset {
+            __typename
+            contentful_id
+            gatsbyImageData(width: 920)
+          }
+        }
       }
     }
   }
 `
 
-export default ComponentName
+export default Page
