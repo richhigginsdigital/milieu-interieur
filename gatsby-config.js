373e6b4a939124635fa6283e29fa32706b8a1e72
richhiggins <hello@iamrichhiggins.com>
2021-03-24 11:29:37 +0000

    added algolia plugin and local gatsby-cli to workaround missing dependency issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,32 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+// gatsby-config.js
+const myQuery = `{
+  pages: allContentfulPage {
+    nodes {
+      objectID:id
+      title
+      slug
+      node_locale
+      updatedAt
+    }
+  }
+}`
+
+const queries = [
+  {
+    query: myQuery,
+    transformer: ({ data }) => data.pages.nodes, // optional
+    //    indexName: "index name to target", // overrides main index name, optional
+    //    settings: {
+    // optional, any index settings
+    // Note: by supplying settings, you will overwrite all existing settings on the index
+    //    },
+    //    matchFields: ["slug", "modified"], // Array<String> overrides main match fields, optional
+  },
+]
+
 module.exports = {
   siteMetadata: {
     title: `Milieu Intérieur`,
@@ -66,5 +92,26 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
+        // Tip: use Search API key with GATSBY_ prefix to access the service from within components
+        apiKey: process.env.ALGOLIA_API_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
+        queries,
+        chunkSize: 10000, // default: 1000
+        settings: {
+          // optional, any index settings
+          // Note: by supplying settings, you will overwrite all existing settings on the index
+        },
+        enablePartialUpdates: true, // default: false
+        matchFields: ["updatedAt"], // Array<String> default: ['modified']
+        concurrentQueries: false, // default: true
+        skipIndexing: false, // default: false, useful for e.g. preview deploys or local development
+      },
+    },
   ],
 }
