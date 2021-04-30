fcec34cdc9cd2b542ca993844f27789c1c99db3a
richhiggins <hello@iamrichhiggins.com>
2021-04-30 15:28:04 +0100

    made logos an unordered list

--- src/components/consortiumLogos.js
+++ src/components/consortiumLogos.js
@@ -2,13 +2,13 @@ import * as React from "react"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 const ConsortiumLogos = ({ logos }) => (
-  <div className="grid-logos">
+  <ul className="grid-logos">
     {logos.map(logo => (
-      <div>
+      <li>
         <GatsbyImage alt={logo.description} image={logo.gatsbyImageData} />
-      </div>
+      </li>
     ))}
-  </div>
+  </ul>
 )
 
 export default ConsortiumLogos
