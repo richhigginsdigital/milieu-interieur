2800663c09cec0c82a8b4a2500c5c7118923cec6
richhiggins <hello@iamrichhiggins.com>
2021-05-04 15:49:30 +0100

    tidied up collaborateBanner

--- src/components/collaborateBanner.js
+++ src/components/collaborateBanner.js
@@ -1,64 +1,38 @@
 import React, { useState } from "react"
 import { ReactComponent as CopyIcon } from "../images/copy-icon.svg"
+import "./collaborateBanner.css"
 
 const CollaborateBanner = () => {
   const [copied, setCopied] = useState(false)
 
-  const copyToClipboard = () => {
+  const copyToClipboard = event => {
+    event.target.select()
     navigator.clipboard.writeText("milieuinterieur@pasteur.fr")
     setCopied(true)
   }
 
   return (
-    <div
-      style={{
-        background: "#EB924A",
-        overflow: "hidden",
-        borderTop: "1px solid black",
-        padding: "1.5rem 0",
-      }}
-    >
-      <div className="l-constrained" style={{ position: "relative" }}>
+    <div className="collaborate-banner">
+      <div className="l-constrained">
         <h2>How to collaborate with us</h2>
-        <p style={{ maxWidth: "680px" }}>
+        <p>
           Researchers can obtain access to the Milieu Intérieur data, biological
           samples and methodology by submitting a research proposal to:{" "}
-          <span
-            style={{
-              background: "white",
-              padding: "7px 24px",
-              position: "absolute",
-              right: 0,
-              top: "2.5rem",
-              border: "1px solid black",
-              fontSize: "1rem",
-            }}
-          >
-            milieuinterieur@pasteur.fr
-            <span
-              onClick={() => copyToClipboard()}
-              title="Copy to clipboard"
-              style={{
-                cursor: "pointer",
-                verticalAlign: "middle",
-                marginLeft: ".5rem",
-              }}
-            >
-              <CopyIcon />
-            </span>
-            {copied && (
-              <span
-                style={{
-                  position: "absolute",
-                  right: 0,
-                  marginTop: "2.5rem",
-                }}
-              >
-                Copied to clipboard
-              </span>
-            )}
-          </span>
         </p>
+        <div className="copy-button">
+          <label htmlFor="copylink" title="Copy to clipboard">
+            <CopyIcon />
+          </label>
+          <input
+            type="text"
+            readOnly={true}
+            value="milieuinterieur@pasteur.fr"
+            onClick={copyToClipboard}
+            onBlur={() => setCopied(false)}
+            id="copylink"
+          ></input>
+          <div>{copied && "Copied to clipboard"}</div>
+        </div>
       </div>
     </div>
   )
