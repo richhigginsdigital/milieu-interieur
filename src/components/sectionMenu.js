db8875dfbb7d69af13cb0bd8596b7a21c778e5b4
richhiggins <hello@iamrichhiggins.com>
2021-06-09 15:15:11 +0100

    WIP on section menu styling

--- src/components/sectionMenu.js
+++ src/components/sectionMenu.js
@@ -3,16 +3,18 @@ import PropTypes from "prop-types"
 
 import { pageLink } from "../helpers/pageLink"
 
+import "./sectionMenu.css"
+
 const SectionMenu = ({ pages }) => (
   <>
     <div class="section-menu">
       <nav>
         <ul>
           {pages.map(page => (
-            <li style={{ marginBottom: ".725rem" }}>
+            <li>
               {pageLink(page.node)}
 
-              {page.node.childPages && (
+              {page.node.childPages.length > 0 && (
                 <ul style={{ marginTop: ".725rem" }}>
                   {page.node.childPages.map(subPage => (
                     <li style={{ marginBottom: ".725rem" }}>
