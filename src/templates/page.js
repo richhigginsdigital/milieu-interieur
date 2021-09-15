4124fc2d94e3ab5266432de11f47b5ca9fd7e63d
richhiggins <hello@iamrichhiggins.com>
2021-09-15 10:02:14 +0100

    added anchor to heading level 2

--- src/templates/page.js
+++ src/templates/page.js
@@ -24,6 +24,17 @@ const Page = ({ data, location, pageContext }) => {
       ? true
       : false
 
+  function slugify(text) {
+    return text
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, "-") // Replace spaces with -
+      .replace(/[^\w-]+/g, "") // Remove all non-word chars
+      .replace(/--+/g, "-") // Replace multiple - with single -
+      .replace(/^-+/, "") // Trim - from start of text
+      .replace(/-+$/, "") // Trim - from end of text
+  }
+
   return (
     <Layout
       locale={locale}
@@ -155,6 +166,11 @@ const Page = ({ data, location, pageContext }) => {
                   </div>
                 )
               },
+              [BLOCKS.HEADING_2]: node => (
+                <h2 id={slugify(node.content[0].value)}>
+                  {node.content[0].value}{" "}
+                </h2>
+              ),
             },
             renderText: text =>
               text
